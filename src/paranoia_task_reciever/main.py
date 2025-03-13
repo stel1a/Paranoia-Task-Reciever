a5c40c3e2263d9c36664fc29ec387f599421c842
stella :3c <hey@stels.sh>
2025-03-13 08:56:02 -0400

    added template tasks and fixed test printer override

--- src/paranoia_task_reciever/main.py
+++ src/paranoia_task_reciever/main.py
@@ -1,25 +1,26 @@
 import os
-from escpos import * 
+from escpos import *
 
 from .utils.ui import summon_ui, show_tutorial, create_tasks_folder, new_printer
 from rich.console import Console
 
 def main() -> None:
-    testing = False
+    testing = True
 
     console = Console()
 
-    if testing: p = printer.Dummy()
-    
     if not os.path.exists("./tasks/"):
         create_tasks_folder(console)
 
-    if not os.path.exists("./.printer"):
-        p = new_printer(console)
+    if testing: p = printer.Dummy()
     else:
-        f = open("./.printer", "r").read().split(",")
-        opt = list(map(int, f))
-        p = printer.Usb(opt[0], opt[1])
+        if not os.path.exists("./.printer"):
+            p = new_printer(console)
+        else:
+            f = open("./.printer", "r").read().split(",")
+            opt = list(map(int, f))
+            p = printer.Usb(opt[0], opt[1])
+
     if not os.path.exists("./tasks/.tutorial_shown"):
         show_tutorial(console)
 
